6a8277233334f5e5d02176392da1592a01782664
Brutotes <48693133+Dfpello@users.noreply.github.com>
2025-11-06 17:54:07 +0100

    Modificada la clase de User para Tener getters y setters de los atributos del usuario

--- src/main/java/es/grise/upm/profundizacion/subscriptionService/User.java
+++ src/main/java/es/grise/upm/profundizacion/subscriptionService/User.java
@@ -1,10 +1,17 @@
 package es.grise.upm.profundizacion.subscriptionService;
 
 public class User {
-	
-	Delivery delivery = null;
+	Delivery delivery;
+	String email;
 
-	public String getEmail() { return null;};
-	public Delivery getDelivery() {return null;};
+	public User(Delivery delivery, String email) {
+		this.delivery = delivery;
+		this.email = email;
+	}
+	
+	public void setDelivery(Delivery delivery) { this.delivery = delivery;}
+	public void setEmail(String email) { this.email = email;}
+	public String getEmail() { return email;}
+	public Delivery getDelivery() {return delivery;}
 	
 }
